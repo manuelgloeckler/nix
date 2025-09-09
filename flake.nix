05a51c964e1729522f61597a84c8d3c3140c1a42
Manuel Gloeckler <manug@Mac.fritz.box>
2025-09-09 21:48:41 +0200

    dot file mangement

---
+++ flake.nix
@@ -0,0 +1,177 @@
+{
+  description = "Example nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    mac-app-util.url = "github:hraban/mac-app-util";
+      # NEW:
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    # Optional: Declarative tap management
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+    homebrew-bundle = {
+      url = "github:homebrew/homebrew-bundle";
+      flake = false;
+    };
+    homebrew-services = {
+      url = "github:homebrew/homebrew-services";
+      flake = false;
+    };
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, mac-app-util, home-manager, nix-homebrew, homebrew-core, homebrew-cask, homebrew-bundle, homebrew-services, ... }:
+  let
+    configuration = { pkgs, ... }: {
+      nixpkgs.config.allowUnfree = true;
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = [ 
+        pkgs.git
+        pkgs.vim
+        pkgs.neovim
+        pkgs.obsidian
+        pkgs.vscode
+        pkgs.wget
+	pkgs.ghostty-bin
+	pkgs.neofetch
+	pkgs.uv
+      ];
+
+      nixpkgs.config.allowBroken = true;
+
+      
+      homebrew = {
+        enable = true;
+
+        # Uncomment to install cli packages from Homebrew.
+        brews = [
+           "mas"
+        ];
+
+        # Uncomment to install cask packages from Homebrew.
+        casks = [
+	   "zoom"
+	   "inkscape"
+	   "karabiner-elements"
+        ];
+
+        # Uncomment to install app store apps using mas-cli.
+        masApps = {
+           "Microsoft Word"       = 462054704;
+           "Microsoft Excel"      = 462058435;
+           "Microsoft PowerPoint" = 462062816;
+           "Microsoft OneNote"    = 784801555;
+           "OneDrive"             = 823766827;   # optional
+        };
+
+        # Uncomment to automatically update Homebrew and upgrade packages.
+        onActivation.autoUpdate = true;
+        onActivation.upgrade = true;
+      };
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+      programs.zsh.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+      system.primaryUser = "manug";
+
+      system.defaults = {
+        dock.autohide  = true;
+	dock.persistent-apps = [
+        	"${pkgs.ghostty-bin}/Applications/Ghostty.app"
+		"/Applications/Google Chrome.app"
+		"${pkgs.obsidian}/Applications/Obsidian.app"
+		"/System/Applications/Mail.app"
+		"/System/Applications/Calendar.app"
+	];
+        dock.magnification = false;
+        dock.mineffect = "genie";
+        finder.FXPreferredViewStyle = "clmv";
+        loginwindow.GuestEnabled  = false;
+        NSGlobalDomain.AppleInterfaceStyle = "Dark";
+        NSGlobalDomain.KeyRepeat = 2;
+      };
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#simple
+    darwinConfigurations."mac" = nix-darwin.lib.darwinSystem {
+      modules = [ 
+        configuration
+        mac-app-util.darwinModules.default
+        nix-homebrew.darwinModules.nix-homebrew
+	home-manager.darwinModules.home-manager
+        {   
+	  system.primaryUser = "manug";
+
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+	  home-manager.backupFileExtension = "backup"; 
+	  # Your HM config for user manug goes here:
+          home-manager.users."manug" = { pkgs, lib, config, ... }: {
+              # Required on macOS:
+              home.username = "manug";
+              home.homeDirectory = lib.mkForce "/Users/manug";
+              home.stateVersion = "24.11";  # lock HM state (pick your HM version)
+
+
+              # Example: write Karabiner config declaratively
+	      xdg.configFile."karabiner/karabiner.json".source = ./dotfiles/karabiner/karabiner.json;
+	      home.file.".gitconfig".source = ./dotfiles/git/.gitconfig;
+	      xdg.configFile."ghostty/config".source = ./dotfiles/ghostty/config;
+          };
+
+          nix-homebrew = {
+            # Install Homebrew under the default prefix
+            enable = true;
+	    
+            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+            enableRosetta = true;
+
+            # User owning the Homebrew prefix
+            user = "manug";
+
+            # Optional: Declarative tap management
+            taps = {
+              "homebrew/homebrew-core" = homebrew-core;
+              "homebrew/homebrew-cask" = homebrew-cask;
+              "homebrew/homebrew-bundle" = homebrew-bundle;
+              "homebrew/homebrew-services" = homebrew-services;
+            };
+
+            # Optional: Enable fully-declarative tap management
+            #
+            # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+            mutableTaps = false;
+
+            # Automatically migrate existing Homebrew installations
+            # autoMigrate = true;
+          };
+        }
+      ];
+    };
+  };
+}
